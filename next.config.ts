7e831198562d99370a90797d1b05199c363fa730
vikash1551 <vikashkumar221005@gmail.com>
2026-09-14 15:52:38 +0530

    fix build error

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
+
 };
 
 export default nextConfig;
